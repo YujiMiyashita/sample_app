9d3c5c4add3b0f1d78721ea24c9a5a5064790604
Yuji Miyashita <ikkiiimonkey0229@gmail.com>
2019-02-28 17:04:09 +0900

    csrf off.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  
+  # CSRF OFF
+  config.action_controller.allow_forgery_protection = false
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
